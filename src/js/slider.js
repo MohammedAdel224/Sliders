995380143468c28f6125f54cadda827344173cf2
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-11 17:08:34 +0200

    implement slider class

---
+++ src/js/slider.js
@@ -0,0 +1,48 @@
+import { assertHtmlElement, toPixel } from "../util.js"
+
+class Slider{
+    #slider;
+
+    constructor(element){
+        assertHtmlElement(element);
+        this.#slider = element;
+    }
+
+    toElement(){
+        return this.#slider;
+    }
+
+    addEventListener(type, listener){
+        this.#slider.addEventListener(type, listener);
+    }
+
+    setValue(value){
+        this.#slider.value = value;
+    }
+
+    get value(){
+        return this.#slider.value;
+    }
+
+    get min(){
+        return this.#slider.min | 0;
+    }
+
+    get max(){
+        return  this.#slider.max | 100;
+    }
+
+    get width(){
+        const style = getComputedStyle(this.#slider);
+        const width = style.getPropertyValue('width');
+        return parseFloat(toPixel(width));
+    }
+
+    get thumbWidth(){
+        const style = getComputedStyle(this.#slider);
+        const thumbWidth = style.getPropertyValue('--thumb-width');
+        return parseFloat(toPixel(thumbWidth));
+    }
+}
+
+export default Slider;
