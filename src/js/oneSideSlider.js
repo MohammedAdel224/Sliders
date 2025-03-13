4b2484882749e23b35555b00784401853b0cbe74
Mohammed Adel <mohammedadellma224@gmail.com>
2025-03-13 09:40:51 +0200

    fix fill track to be align with the center of the thumb

--- src/js/oneSideSlider.js
+++ src/js/oneSideSlider.js
@@ -21,8 +21,12 @@ class OneSideSlider{
     }
 
     #fillTrack(){
-        const percent = (this.#slider.value - this.#slider.min) / (this.#slider.max - this.#slider.min) * 100;
-        this.#track.fill(percent);
+        const trackLength = this.#track.width;
+        const thumbTrackLength = this.#track.width - this.#slider.thumbWidth;
+        const percent = (this.#slider.value - this.#slider.min) / (this.#slider.max - this.#slider.min);
+        const offset = (trackLength - thumbTrackLength) / (2 * trackLength); 
+        const adjustedPercent = thumbTrackLength / trackLength * percent + offset;
+        this.#track.fill(adjustedPercent * 100);
     }
 }
 
